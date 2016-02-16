ef07cfe322cf14df7af404d023f6cd263dd70006
barbatus <alex.borodach@gmail.com>
2016-02-16 23:46:15 +0800

    Update package.js

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   name: 'barbatus:ts-compilers',
-  version: '0.2.8_4',
+  version: '0.2.8_5',
   summary: 'TypeScript Compilers for Meteor',
-  git: 'https://github.com/barbatus/ts-compilers',
+  git: 'https://github.com/barbatus/typesctipt-compiler/tree/ts-compilers',
   documentation: 'README.md'
 });
 
@@ -22,7 +22,7 @@ Package.onUse(function(api) {
     'ecmascript@0.1.4',
     'check@1.0.5',
     'underscore@1.0.4',
-    'barbatus:typescript@0.1.5'
+    'barbatus:typescript@0.1.5_1'
   ], server);
 
   api.addFiles([
