b5f36d87e6ef3988796502c1a4d907278dfc0e01
barbatus <alex.borodach@gmail.com>
2016-04-12 14:16:53 +0300

    Bump package version along with the version of the meteor-typescript NPM
    https://github.com/Urigo/angular2-meteor/issues/102

--- package.js
+++ package.js
@@ -1,17 +1,19 @@
 Package.describe({
   name: 'barbatus:typescript-compiler',
-  version: '0.5.0-beta.14',
+  version: '0.5.4',
   summary: 'TypeScript Compiler for Meteor',
   git: 'https://github.com/barbatus/ts-compilers',
   documentation: 'README.md'
 });
 
 Npm.depends({
-  'meteor-typescript': 'https://github.com/barbatus/meteor-typescript/tarball/0bae2a6935bb077f02ad48d359f9eba418052acf',
+  'meteor-typescript': 'https://github.com/barbatus/meteor-typescript/tarball/35661eed0a5cbe017e8213527d144eab61614dbc',
   'async': '1.4.0'
 });
 
 Package.onUse(function(api) {
+  api.versionsFrom('1.2.0.1');
+
   api.use([
     'ecmascript@0.1.6',
     'check@1.0.5',
