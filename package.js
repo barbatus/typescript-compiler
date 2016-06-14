640ea5a7fa1bfedb553fdade97cb5718d14ed22f
barbatus <alex.borodach@gmail.com>
2016-06-14 13:15:37 +0300

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'barbatus:typescript-compiler',
-  version: '0.6.0',
+  version: '0.6.1',
   summary: 'TypeScript Compiler for Meteor',
   git: 'https://github.com/barbatus/ts-compilers',
   documentation: 'README.md'
